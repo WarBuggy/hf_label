bfa41aba9dfc8c6233e04a79951ecbf9426d8ff1
hovanbuu <hovanbuu@gmail.com>
2021-09-10 14:07:58 +0700

    new label

--- label.js
+++ label.js
@@ -1,6 +1,20 @@
 module.exports = {
-    product: {
-        vi: 'sản phẩm',
-        en: 'product',
+    email_phone: {
+        vi: 'Email',
     },
-}
+    password: {
+        vi: 'Mật khẩu',
+    },
+    login: {
+        vi: 'Đăng nhập',
+    },
+    register: {
+        vi: 'Đăng ký',
+    },
+    forgot_password: {
+        vi: 'Quên mật khẩu',
+    },
+    wrong_username_or_password: {
+        vi: 'Email hay mật khẩu không đúng.',
+    },
+};
