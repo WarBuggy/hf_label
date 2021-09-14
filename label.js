754dd7c33539323d119674cbc9ccbe4ee36eddfa
hovanbuu <hovanbuu@gmail.com>
2021-09-14 13:47:34 +0700

    updat

--- label.js
+++ label.js
@@ -1,4 +1,4 @@
-module.exports = {
+let data = {
     email_phone: {
         vi: 'Email',
     },
@@ -17,4 +17,13 @@ module.exports = {
     wrong_username_or_password: {
         vi: 'Email hay mật khẩu không đúng.',
     },
+    login_remember: {
+        vi: 'Nhớ thông tin đăng nhập',
+    },
 };
+
+if (typeof module == 'undefined' || typeof module.exports == 'undefined') {
+    window.label = data;
+} else {
+    module.exports = data;
+}
