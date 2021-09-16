36f49c67bfb747d73d2457f418c1ba216f438a64
hovanbuu <hovanbuu@gmail.com>
2021-09-17 00:16:25 +0700

    update

--- label.js
+++ label.js
@@ -24,6 +24,31 @@ let data = {
     error_api_general: {
         vi: 'Hệ thống gặp lỗi {0}.\nXin vui lòng thử lại sau!',
     },
+
+    send_email_confirm_code: {
+        vi: 'Gửi mã.',
+    },
+    require_password_strong: {
+        vi: 'Mật khẩu cần ít nhất 8 ký tự, ít nhất 1 chữ thường, 1 chữ hoa, 1 số và 1 ký tự đặc biệt.',
+    },
+
+    require_email_taken: {
+        vi: 'Email đã được sử dụng cho tài khoản khác.',
+    },
+    require_email_confirm: {
+        vi: 'Nhập mã xác nhận email. Nếu bạn chưa có, bấm nút "Gửi mã" và kiểm hộp thư liên quan.',
+    },
+
+    welcome_new_registered_user: {
+        vi: 'Xin chào mừng quý khách đã gia nhập cộng đồng happynessfunds.com. Chúng tôi luôn cố gắng hết mình để quý khách có được những trải nghiệm mua sắm tuyệt vời nhất.',
+    },
+    continue: {
+        vi: 'Tiếp tục',
+    },
+
+    title_register: {
+        vi: 'Đăng ký tài khoản phổ thông',
+    },
 };
 
 if (typeof module == 'undefined' || typeof module.exports == 'undefined') {
